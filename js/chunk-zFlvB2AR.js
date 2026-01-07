b652bb0d766a311d6d89600a88cadd9420c53810
Saraph1nes <Saraph1nes@users.noreply.github.com>
2026-01-07 15:15:06 +0000

    deploy: e60f79dffea5912a92c6c9fdbb4c10d3d81ee6c9

---
+++ js/chunk-zFlvB2AR.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./chunk-CXRn-VD0.js";import{J as o,o as s,I as t}from"./chunk-D-eIezFx.js";const n=r({},[["render",function(r,n){const e=t("router-view");return s(),o(e)}]]);export{n as default};
